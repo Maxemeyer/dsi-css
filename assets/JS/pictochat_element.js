9036f8c2fab44ffe92648b3c250d26ec96eeb1e4
Maxemexer <maximilianmryka@gmail.com>
2026-06-28 19:38:49 +0200

    added canvas element to fieldset
    added canvas drawing tool tutorials to links.txt list
    
    moved css from pictochat_element.html to pictochat_element.css
    
    canvas minor styling
    
    all important values in variables under :root
    
    every value was multiplied by 5

--- assets/JS/pictochat_element.js
+++ assets/JS/pictochat_element.js
@@ -1,17 +1,18 @@
 const fieldset = document.querySelector('fieldset');
 const legend = fieldset.querySelector('legend');
-const textarea = fieldset.querySelector('textarea');
 const span = legend.querySelector('span');
 const input = legend.querySelector('input');
+const textarea = fieldset.querySelector('textarea');
+const canvas = fieldset.querySelector('canvas');
 
 function responsiveIndent() {
-    let indent = legend.getBoundingClientRect().width - 1;
+    let indent = legend.getBoundingClientRect().width;
     textarea.style.textIndent = indent + 'px';
 }
 
 function syncInputWidth() {
     span.textContent = input.value || input.placeholder;
-    let width = span.getBoundingClientRect().width;
+    let width = span.getBoundingClientRect().width + 5;
     input.style.width = width + 'px';
 
     responsiveIndent();
@@ -19,9 +20,7 @@ function syncInputWidth() {
 
 function forceMaxLength() {
     if (textarea.scrollHeight !== textarea.offsetHeight) {
-        console.log("statement check");
-
-        document.execCommand('undo');
+        document.execCommand('delete');
     }
 }
 
